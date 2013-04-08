b9d962bc711968935e1978ffa1ea6202cce041ed
Andrew McCloud <andrew@amccloud.com>
2013-04-07 23:26:47 -0700

    Fix for legacy images.

--- public/js/models/zappos/products.js
+++ public/js/models/zappos/products.js
@@ -24,19 +24,38 @@ define(function(require) {
             return src.replace(from, to);
         },
         _getThumbnailImage: function(imageSrc) {
-            return this._convertImageRecipe(imageSrc,
+            var thumbnailSrc = this._convertImageRecipe(imageSrc,
                 ZapposProduct.IMAGE_RECIPE_THUMBNAIL,
                 ZapposProduct.IMAGE_RECIPE_MULTIVIEW);
+
+            if (thumbnailSrc.indexOf(ZapposProduct.IMAGE_RECIPE_MULTIVIEW) === -1) {
+                thumbnailSrc = this._convertImageRecipe(imageSrc,
+                    ZapposProduct.IMAGE_RECIPE_THUMBNAIL_LEGACY,
+                    ZapposProduct.IMAGE_RECIPE_MULTIVIEW_LEGACY);
+            }
+
+            return thumbnailSrc;
         },
         _getFullImage: function(imageSrc) {
-            return this._convertImageRecipe(imageSrc,
+            var fullSrc = this._convertImageRecipe(imageSrc,
                 ZapposProduct.IMAGE_RECIPE_THUMBNAIL,
                 ZapposProduct.IMAGE_RECIPE_4X);
+
+            if (fullSrc.indexOf(ZapposProduct.IMAGE_RECIPE_4X) === -1) {
+                fullSrc = this._convertImageRecipe(imageSrc,
+                    ZapposProduct.IMAGE_RECIPE_THUMBNAIL_LEGACY,
+                    ZapposProduct.IMAGE_RECIPE_4X_LEGACY);
+            }
+
+            return fullSrc;
         }
     }, {
-        IMAGE_RECIPE_THUMBNAIL: 't-THUMBNAIL',
-        IMAGE_RECIPE_MULTIVIEW: 'p-MULTIVIEW',
-        IMAGE_RECIPE_4X: 'p-4x'
+        IMAGE_RECIPE_THUMBNAIL: 't-THUMBNAIL.jpg',
+        IMAGE_RECIPE_MULTIVIEW: 'p-MULTIVIEW.jpg',
+        IMAGE_RECIPE_4X: 'p-4x.jpg',
+        IMAGE_RECIPE_THUMBNAIL_LEGACY: 't.jpg',
+        IMAGE_RECIPE_MULTIVIEW_LEGACY: 'p.jpg',
+        IMAGE_RECIPE_4X_LEGACY: 'p.jpg'
     });
 
     return Products.extend({
